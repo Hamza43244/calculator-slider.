9598ffcffac1d18af9fc2c11e3a29ae8d253a78c
Hamza43244 <107225325+Hamza43244@users.noreply.github.com>
2022-07-28 05:29:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// calculator
+var isCalculated;
+function getNumber(num){
+    
+    var result = document.getElementById("result")
+   if (isCalculated){
+    clearResult();
+   }
+
+    result.value += num;
+    isCalculated = false;
+
+}
+
+function clearResult(){
+    var result = document.getElementById("result")
+    result.value = " ";
+}
+function getResult(){
+  isCalculated = true;
+    var result = document.getElementById("result")
+   result.value = eval(result.value)
+}
+
+// slider
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
